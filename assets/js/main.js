2956354ca74fd1220fe3a73eba7d42b600ce4c85
rupertviglione <goncalorodrigues.nuno@gmail.com>
2025-07-25 10:46:36 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,30 +128,20 @@
   /**
    * Hero type effect
    */
-  const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
-  const typeded = select('.typeded')
-  if (typeded) {
-    let typed_strings = typeded.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typeded', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+  const typed_elements = select('.typed', true);
+  typed_elements.forEach(typed => {
+    if (typed) {
+      let typed_strings = typed.getAttribute('data-typed-items');
+      typed_strings = typed_strings.split(',');
+      new Typed(typed, {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+      });
+    }
+  });
 
   /**
    * Skills animation
